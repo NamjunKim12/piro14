d0a0db2fdcc00423bd3e5bb687ccb5a58b4f8133
NamjunKim12 <wrasf175@gmail.com>
2021-01-15 18:16:17 +0900

    Askcompany장고5강

---
+++ Python_AskdjangoBasic/askcompany/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/user/Desktop/피로그래밍 개인과제/piro14/Python_AskdjangoBasic/askcompany/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
