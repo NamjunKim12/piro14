b87f49d44454398d83d7a6477e47ad2b703ca9e0
NamjunKim12 <wrasf175@gmail.com>
2021-01-16 23:54:04 +0900

    Askcompany 장고 16과

--- Python_AskdjangoBasic/askcompany/blog/urls.py
+++ Python_AskdjangoBasic/askcompany/blog/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
 
 app_name ='blog'
@@ -6,4 +7,11 @@ app_name ='blog'
 urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('blog/', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
