81e79043809cabc984697ef216c647c5dfc72b68
NamjunKim12 <wrasf175@gmail.com>
2021-01-19 04:00:31 +0900

    중급편) Askdjango 7,8강

--- django-auth/accounts/views.py
+++ django-auth/accounts/views.py
@@ -1,16 +1,25 @@
 from django.conf import settings
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, resolve_url
 from .forms import SignupForm
 
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
 
-signup = CreateView.as_view(model=User,
-        form_class=SignupForm,
-        success_url=settings.LOGIN_URL,
-        template_name='accounts/signup.html')
+    def get_success_url(self):
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+signup = SignupView.as_view()
 
 @login_required
 def profile(request):
